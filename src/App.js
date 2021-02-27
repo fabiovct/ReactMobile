5d5541cfcfa918c7391f6da671bcd6131642f1b9
fabiovct <vct.fabio.vct@gmail.com>
2021-02-27 15:41:51 -0300

    instalando pacotes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import  {NavigationContainer} from '@react-navigation/native'
-import  {createStackNavigator} from '@react-navigation/stack'
+// import  {NavigationContainer} from '@react-navigation/native'
+// import  {createStackNavigator} from '@react-navigation/stack'
 import UserList from './views/UserList'
 import UserForm from './views/UserForm'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import {Button, Icon } from 'react-native-elements';
 
 const Stack = createStackNavigator()
 
@@ -10,16 +13,40 @@ export default props => {
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName="UserList">
+                initialRouteName="UserList"
+                screenOptions={screenOptions}
+                >
                 <Stack.Screen
                  name="UserList"
                  component={UserList}
+                 options={({navigation})=>{
+                     return {
+                         title:"Lista de Usuários",
+                         headerRight: () => (
+                             <Button
+                             onPress={() => navigation.navigate("UserForm")}
+                             type="clear"
+                             icon={<Icon name="add" size={25} color="white"/>}
+                             />
+                         )
+                     }
+                }}
                 />
                 <Stack.Screen
                  name="UserForm"
                  component={UserForm}
+                 options={{
+                     title:"Formulário de Usuários"
+                 }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor: '#fff'
 }
