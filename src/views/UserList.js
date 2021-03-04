aa4810b50efc35846fa42aa016cdab9a3c0c87f6
fabiovct <vct.fabio.vct@gmail.com>
2021-03-04 20:26:07 -0300

    alert delete function

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,40 @@
 import React from 'react'
-import {FlatList, Text, View} from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import {Alert, FlatList, Text, View } from 'react-native'
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
 import users from '../data/users'
 
 export default props => {
+
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário','Deseja excluir o usuario'+user.id+'?',[
+            {
+                text: 'Sim',
+                onPress(){
+                    console.warn('delete' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+function getActions(user){
+    return (
+        <>
+            <Button onPress={() => props.navigation.navigate('UserForm', user)} 
+            type="clear"
+            icon={<Icon name="edit" size={25} color="orange"/>}
+            />
+            <Button onPress={() => confirmUserDeletion(user)} 
+            type="clear"
+            icon={<Icon name="delete" size={25} color="red"/>}
+            />
+        </>
+    )
+}
+
+
     function getUserItem({item}) {
         return (
             // <ListItem
@@ -14,11 +45,12 @@ export default props => {
             // bottomDivider
             // onPress={() => props.navigation.navigate('UserForm')}
             // />
-            <ListItem key={item.id} onPress={() => props.navigation.navigate('UserForm')} >
+            <ListItem rightElement={getActions(item)} key={item.id} onPress={() => props.navigation.navigate('UserForm')} >
                 <Avatar source={{uri: item.avatarUrl}} />
-
-                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Title >{item.name}</ListItem.Title>
                 <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+                <ListItem >{getActions(item)}</ListItem>
+                {/* <ListItem.Content>{getActions(user)}</ListItem.Content> */}
             </ListItem>
         )
     }
